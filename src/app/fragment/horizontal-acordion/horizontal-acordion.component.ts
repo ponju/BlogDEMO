818d949d71a8a2b0460867af10f822b1897ff8ac
ponju <ponzujure39@gmail.com>
2021-09-30 18:40:03 +0900

    水平方向追加

--- src/app/fragment/horizontal-acordion/horizontal-acordion.component.ts
+++ src/app/fragment/horizontal-acordion/horizontal-acordion.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractTogglable } from '../abstract-togglable/abstract-togglable';
+import { horisontalAcordion } from '../angular-animations/acordion';
 
 @Component({
-  selector: 'app-horizontal-acordion',
+  selector: 'h-acordion',
   templateUrl: './horizontal-acordion.component.html',
-  styleUrls: ['./horizontal-acordion.component.scss']
+  styleUrls: ['./horizontal-acordion.component.scss'],
+  animations:[
+    horisontalAcordion
+  ]
 })
-export class HorizontalAcordionComponent implements OnInit {
-
-  constructor() { }
+export class HorizontalAcordionComponent implements AbstractTogglable,OnInit {
+  active=false;
+  constructor(){}
+  toggle(): void {
+   this. active=!this.active;
+  }
+  turnON(): void {
+    this.active=true;
+  }
+  turnOFF(): void {
+    this.active=false;
+  }
 
   ngOnInit(): void {
   }
-
 }
